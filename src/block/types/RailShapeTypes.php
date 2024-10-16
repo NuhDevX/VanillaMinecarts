74316defaad5e7efa3f44ff1e7e03ce8fc0da287
pixelwhiz <daffa01futn@gmail.com>
2024-10-16 16:24:03 +0700

    initial commit

--- src/block/types/RailShapeTypes.php
+++ src/block/types/RailShapeTypes.php
@@ -2,6 +2,25 @@
 
 namespace pixelwhiz\vanillaminecarts\block\types;
 
+
+/**
+ * Check using $block->getShape() or $rail->getShape() $block or $rail must be pocketmine\block\Rail
+ * after that u can check getShape() function
+ * @method Rail getShape()
+ *
+ * @var int STRAIGHT_NORTH_SOUTH
+ * @var int SLOPED_ASCENDING_NORTH
+ * @var int SLOPED_DESCENDING_SOUTH
+ * @var int STRAIGHT_EAST_WEST
+ * @var int SLOPED_ASCENDING_EAST
+ * @var int SLOPED_DESCENDING_WEST
+ * @var int CURVED_SOUTH_EAST
+ * @var int CURVED_SOUTH_WEST
+ * @var int CURVED_NORTH_EAST
+ * @var int CURVED_NORTH_WEST
+ *
+ */
+
 class RailShapeTypes {
 
     public const STRAIGHT_NORTH_SOUTH = 0;
@@ -14,10 +33,8 @@ class RailShapeTypes {
     public const SLOPED_DESCENDING_WEST = 3;
 
 
-
     public const CURVED_SOUTH_EAST = 9;
     public const CURVED_SOUTH_WEST = 8;
-
 
 
     public const CURVED_NORTH_EAST = 6;
